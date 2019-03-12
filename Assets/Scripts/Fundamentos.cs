40511db4c829d5b76888d9314ddd10967e037204
Guilherme Galarça Dutra <udutra@gmail.com>
2019-03-12 17:29:56 -0300

    Aula 5
    Aula 5 finalizada.

---
+++ Assets/Scripts/Fundamentos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fundamentos : MonoBehaviour
+{
+    //tipoInformação    nomeVariavel
+    int inteiro = 100;                      //uma variável do tipo inteiro armazena números inteiros, negativo ou positivo. Sendo -2147483648 a 2147483647
+    float flutuante = 1.5f;                 //ponto flutuante, números com casas decimais. 10^38
+    bool isLogico = false;                  //valor lógico, true ou false
+
+    string texto = "Hello World! 1.0";      //armazena qualquer valor alpha numérico
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
