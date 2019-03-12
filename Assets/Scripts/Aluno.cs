fc80fdbe6f8fb5e982150ef1e9296eca27689339
Guilherme Galarça Dutra <udutra@gmail.com>
2019-03-12 17:58:38 -0300

    Aula 6.
    Aula 6 finalizada.

---
+++ Assets/Scripts/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aluno : MonoBehaviour
+{
+
+    //modificador   tipo        nome;
+    public          string      nomeAluno;
+    public          int         idadeAluno;
+    public          bool        isAprovado;
+    public          float       mediaBimestreA;
+    public          float       mediaBimestreB;
+    public          float       mediaBimestreC;
+    public          float       mediaBimestreD;
+    public          float       mediaFinal;
+
+    public void calcularMediaFinal()
+    {
+
+    }
+
+}
