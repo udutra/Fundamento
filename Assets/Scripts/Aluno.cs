38e54ec66edb9711fadfad7dfed22f3d5ae47561
Guilherme Galarça Dutra <udutra@gmail.com>
2019-03-13 18:01:22 -0300

    Aula 9 e 10.
    Aula 9 e 10 finalizadas.

--- Assets/Scripts/Aluno.cs
+++ Assets/Scripts/Aluno.cs
@@ -8,7 +8,7 @@ public class Aluno : MonoBehaviour
     //modificador   tipo        nome;
     public          string      nomeAluno;
     public          int         idadeAluno;
-    private          bool        isAprovado;
+    private          bool       isAprovado;
     public          float       mediaBimestreA;
     public          float       mediaBimestreB;
     public          float       mediaBimestreC;
@@ -24,12 +24,36 @@ public class Aluno : MonoBehaviour
     //protected internal
     //private protected
 
+
     private void Start()
     {
-        calcularMediaFinal();
+        if (mediaFinal >= 7)
+        {
+            print("A média final é maior ou igual a 7");
+        }
+        else if (mediaFinal >= 5)
+        {
+            print("A média final é maior ou igual a 5");
+        }
+        else if (mediaFinal >= 3) {
+            print("A média final é maior ou igual a 3");
+        }
+        else
+        {
+            print("A média final é menor que 3");
+        }
     }
 
-    public void calcularMediaFinal()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Apertei a tecla Espaço");
+            CalcularMediaFinal();
+        }
+    }
+
+    public void CalcularMediaFinal()
     {
         mediaFinal = (mediaBimestreA + mediaBimestreB + mediaBimestreC + mediaBimestreD) / 4;
         print(mediaFinal);
