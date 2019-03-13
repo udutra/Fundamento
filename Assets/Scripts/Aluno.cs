cebe071dbbc95ffb94df1efe669c0510104f5814
Guilherme Galarça Dutra <udutra@gmail.com>
2019-03-13 16:50:13 -0300

    Aula 7
    Aula 7 finalizada

--- Assets/Scripts/Aluno.cs
+++ Assets/Scripts/Aluno.cs
@@ -8,16 +8,24 @@ public class Aluno : MonoBehaviour
     //modificador   tipo        nome;
     public          string      nomeAluno;
     public          int         idadeAluno;
-    public          bool        isAprovado;
+    private          bool        isAprovado;
     public          float       mediaBimestreA;
     public          float       mediaBimestreB;
     public          float       mediaBimestreC;
     public          float       mediaBimestreD;
     public          float       mediaFinal;
 
+    //Modificadores de acesso
+    //public - O acesso não é restrito
+    //private
+
+    //internal
+    //protected
+    //protected internal
+    //private protected
+
     public void calcularMediaFinal()
     {
 
     }
-
 }
---
+++ Assets/Scripts/Exemplo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exemplo : MonoBehaviour
+{
+    private Aluno aluno;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aluno.nomeAluno = "Guilherme";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
