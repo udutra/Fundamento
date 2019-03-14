abf9e7516c048f2694864e6266edca877b8d2239
Guilherme Galarça Dutra <udutra@gmail.com>
2019-03-14 17:42:55 -0300

    Aula 12.
    Aula 12 finalizada.

---
+++ Assets/Scripts/Fundamentos.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fundamentos : MonoBehaviour
+{
+    public int diaSemana; // 0 = domingo
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            EscreverDiaSemana();
+        }
+    }
+
+    private void EscreverDiaSemana()
+    {
+        switch (diaSemana){
+            case 0:
+                {
+                    print("Domingo");
+                    break;
+                }
+            case 1:
+                {
+                    print("Segunda-feira");
+                    break;
+                }
+            case 2:
+                {
+                    print("Terça-feira");
+                    break;
+                }
+            case 3:
+                {
+                    print("Quarta-feira");
+                    break;
+                }
+            case 4:
+                {
+                    print("Quinta-feira");
+                    break;
+                }
+            case 5:
+                {
+                    print("Sexta-feira");
+                    break;
+                }
+            case 6:
+                {
+                    print("Sabado-feira");
+                    break;
+                }
+            default:
+                {
+                    print("Dia da semana inváido");
+                    break;
+                }
+        }
+    }
+}
