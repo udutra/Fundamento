e0905682bea28b945c8c5f672bbb9ed6c8509ddb
Guilherme Galarça Dutra <udutra@gmail.com>
2019-03-21 17:00:22 -0300

    Aula 22, 23 e 24.
    Aula 22, 23 e 24 finalizada.

---
+++ Assets/Scripts/Componentes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Componentes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.name = "Nome do Objeto";
+        print(gameObject.name);
+
+        transform.position = new Vector3(10, 0, 0);
+        transform.localScale = new Vector3(2,2,2);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
