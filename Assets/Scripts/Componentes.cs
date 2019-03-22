68d2ae8b75203ea93753cb7ee1aed00e1b26cb61
Guilherme Galarça Dutra <udutra@gmail.com>
2019-03-22 17:47:21 -0300

    Aula 25 e 26.
    Aula 25 e 26 finalizadas.

--- Assets/Scripts/Componentes.cs
+++ Assets/Scripts/Componentes.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class Componentes : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private SpriteRenderer sRenderer;
+    public Color corSprite;
+    public Material novoMaterial;
+
     void Start()
     {
-        gameObject.name = "Nome do Objeto";
+        /*gameObject.name = "Nome do Objeto";
         print(gameObject.name);
 
         transform.position = new Vector3(10, 0, 0);
-        transform.localScale = new Vector3(2,2,2);
+        transform.localScale = new Vector3(2,2,2);*/
+
+        sRenderer = GetComponent<SpriteRenderer>();
+        //sRenderer.color = new Color(255,0,0,255);
+        sRenderer.color = corSprite;
+
+        sRenderer.material = novoMaterial;
+
+        sRenderer.enabled = false;
         
     }
 
