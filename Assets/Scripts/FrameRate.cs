cc4593c4c9a713b18fdf29d7314e1ccf8052b329
Guilherme Galarça Dutra <udutra@gmail.com>
2019-03-29 11:09:24 -0300

    Aula 33, 34, 35, 36 e 37.
    Aula 33, 34, 35, 36 e 37 finalizada.

---
+++ Assets/Scripts/FrameRate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRate : MonoBehaviour
+{
+    public int numFrame, vsync;
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = vsync;
+        Application.targetFrameRate = numFrame;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    public float velocidadeMovimento, limiteMaxX, limiteMinX, limiteMaxY, limiteMinY;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        float posX = transform.position.x;
+        float posY = transform.position.y;
+
+        playerRb.velocity = new Vector2(horizontal * velocidadeMovimento, vertical * velocidadeMovimento);
+
+        //Verificar limite X
+        if (transform.position.x > limiteMaxX)
+        {
+            posX = limiteMaxX;
+        }
+        else if (transform.position.x < limiteMinX)
+        {
+            posX = limiteMinX;
+        }
+
+
+        //Verificar limite Y
+        if (transform.position.y > limiteMaxY)
+        {
+            posY = limiteMaxY;
+        }
+        else if(transform.position.y < limiteMinY)
+        {
+            posY = limiteMinY;
+        }
+
+        transform.position = new Vector3(posX, posY, 0);
+
+    }
+}
