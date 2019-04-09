5c8d59b9c4a0ce351c23334faeb4d105e07847ae
Guilherme Galarça Dutra <udutra@gmail.com>
2019-04-09 18:29:12 -0300

    Aulas 55 e 56.
    Aulas 55 e 56 finalizadas.

---
+++ Assets/Scripts/AnimatorPersonagem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorPersonagem : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    private Animator playerAnimator;
+    public Transform groundCheck;
+    private bool isGrounded;
+    private int speedX;
+    private float speedY;
+    public float speed;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f);
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (horizontal != 0)
+        {
+            speedX = 1;
+        }
+        else
+        {
+            speedX = 0;
+        }
+        speedY = playerRb.velocity.y;
+        playerRb.velocity = new Vector2(horizontal * speed, speedY);
+    }
+
+    private void LateUpdate()
+    {
+        playerAnimator.SetInteger("speedX", speedX);
+        playerAnimator.SetFloat("speedY", speedY);
+        playerAnimator.SetBool("grounded", isGrounded);
+    }
+}
